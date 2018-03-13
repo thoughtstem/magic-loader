1495f68cfd98b5115f68984ad99f090a76cbbbdb
Stephen <stephen@thoughtstem.com>
2018-03-13 11:34:00 -0700

    first

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define version "0.0.1")
+
+(define deps '())
+
+(define drracket-tool-names (list "Magic Loader"))
+(define drracket-tools (list (list "tool.rkt")))
+
---
+++ tool.rkt
@@ -0,0 +1,133 @@
+#lang racket/base
+
+(require drracket/tool
+         racket/class
+         racket/gui/base
+         racket/unit
+         mrlib/switchable-button
+         setup/dirs
+         racket/file
+         racket/string
+         racket/list)
+
+(provide tool@)
+ 
+ 
+(define tool@
+  (unit
+    (import drracket:tool^)
+    (export drracket:tool-exports^)
+ 
+    (define reverse-button-mixin
+      (mixin (drracket:unit:frame<%>) ()
+        (super-new)
+        (inherit get-button-panel
+                 get-definitions-text
+                 get-current-tab
+                 open-in-new-tab)
+        (inherit register-toolbar-button)
+ 
+        (let ((btn
+               (new switchable-button%
+                    (label "The Magic Loader")
+                    (callback (λ (button)
+                                (load-file )
+                                ))
+                    (parent (get-button-panel))
+                    (bitmap reverse-content-bitmap))))
+          (register-toolbar-button btn #:number 11)
+          (send (get-button-panel) change-children
+                (λ (l)
+                  (cons btn (remq btn l)))))
+                  
+                  
+        (define (load-file)
+          ; Make a frame by instantiating the frame% class
+          (define frame (new frame% [label "Example"]))
+ 
+          ; Make a static text message in the frame
+          (define msg (new message% [parent frame]
+                           [label "Type the name of a file to load it"]))
+
+
+ 
+          (define panel (new horizontal-panel% [parent frame]))
+
+
+          (define text-field (new text-field%
+                                  (label "Text")
+                                  (parent panel)
+                                  (init-value "Field")))
+
+          (new button% [parent panel]
+               [label "Load"]
+               [callback (lambda (button event)
+                           (send msg set-label "Loading..")
+                           (let ([f (locate-file (send text-field get-value))])
+                             (if f
+                                 (open-in-new-tab f)
+                                 (void)))
+                           )])
+ 
+          ; Show the frame by calling its show method
+          (send frame show #t))
+
+        (define (locate-file name)
+          ;TODO: Search through installed packages for demos... 
+          ;TODO: Search in online locations... 
+          ;      Backend?  
+          ;TODO: Search local network?? Get stuff from teachers???
+
+          (define home    (string-append (path->string (find-system-path 'home-dir)) name ".rkt" ))
+          (define desktop (string-append (path->string (find-system-path 'home-dir)) "/Desktop/" name ".rkt" ))
+          (define smw     (string-append (path->string (find-system-path 'home-dir)) "/Desktop/SAVE_MY_WORK" name ".rkt" ))
+
+      
+          ;Note: order these so that it will return the local file with higher priority
+    
+          (or (search-pkgs name)
+              (cond 
+                [(file-exists? smw)     smw]
+                [(file-exists? home)    home]
+                [(file-exists? desktop) desktop]
+                [else #f])
+              ))
+
+        (define (search-pkgs name)
+          (define pkgs_dir  
+                   (path->string (find-user-pkgs-dir)))
+
+          (define files
+            (find-files	 
+                  (λ(x) (string-contains? (path->string x)
+                                          (string-append "/" name ".rkt")))	 
+                  pkgs_dir))
+
+          (if (empty? files)
+              #f
+              (first files)) 
+
+          )
+        ))
+ 
+    (define reverse-content-bitmap
+      (let* ((bmp (make-bitmap 16 16))
+             (bdc (make-object bitmap-dc% bmp)))
+        (send bdc erase)
+        (send bdc set-smoothing 'smoothed)
+        (send bdc set-pen "black" 1 'transparent)
+        (send bdc set-brush "blue" 'solid)
+        (send bdc draw-ellipse 2 2 8 8)
+        (send bdc set-brush "red" 'solid)
+        (send bdc draw-ellipse 6 6 8 8)
+        (send bdc set-bitmap #f)
+        bmp))
+ 
+
+ 
+    (define (phase1) (void))
+    (define (phase2) (void))
+    
+    (drracket:get/extend:extend-unit-frame reverse-button-mixin)))
+
+
